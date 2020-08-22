450451ec95a7cef9d9cbeb9025ba5649a5ea728f
Joel Fillmore <joelfillmore@gmail.com>
2020-08-21 19:30:46 -0700

    Fix API requests

--- sources/ProjectApiSource.js
+++ sources/ProjectApiSource.js
@@ -15,39 +15,45 @@ class ProjectClient {
             throw new Error('Missing project API key');
         }
 
+        this.apiKey = apiKey;
         this.projectId = projectId;
         this.apiClient = axios.create({
             baseURL: apiUrl + 'projects/' + this.projectId + '/',
-            params: {
-                apiKey: apiKey,
-            },
             timeout: timeout,
         });
     }
 
+    async _apiGetRequest(url) {
+        return this.apiClient.get(url, {
+            params: {
+                apiKey: this.apiKey,
+            },
+        });
+    }
+
     async getPublishedReleaseVersion() {
-        const response = await this.apiClient.get(`releases/published/latest`);
+        const response = await this._apiGetRequest(`releases/published/latest`);
         const releaseSummary = response.data;
         return releaseSummary ? releaseSummary.version : null;
     }
 
     async getPublishedRelease(version) {
-        const response = await this.apiClient.get(`releases/published/${version}/manifest`);
+        const response = await this._apiGetRequest(`releases/published/${version}/manifest`);
         return response.data;
     }
 
     async getScheduledRelease(scheduledId) {
-        const response = await this.apiClient.get(`releases/scheduled/${scheduledId}/manifest`);
+        const response = await this._apiGetRequest(`releases/scheduled/${scheduledId}/manifest`);
         return response.data;
     }
 
     async getSavedRelease() {
-        const response = await this.apiClient.get(`releases/saved/manifest`);
+        const response = await this._apiGetRequest(`releases/saved/manifest`);
         return response.data;
     }
 
     async getItem(collectionId, itemId, itemVersion) {
-        const response = await this.apiClient.get(
+        const response = await this._apiGetRequest(
             `collections/${collectionId}/items/${itemId}/${itemVersion}`
         );
         return response.data;
